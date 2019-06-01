36723093763bacdb7119f8023161e25e57c42ab6
ruohoruotsi <ruoho.ruotsi@gmail.com>
2019-06-01 13:16:28 -0700

    [RM] python3.6 from scripts, just python3, code has been tested on python {3.5, 3.5, 3.7}

--- 02_run_prediction.sh
+++ 02_run_prediction.sh
@@ -4,7 +4,7 @@ echo "[INFO] running inference on test sources"
 
 # Note that in lieu of: -model models/yo_adr_bahdanau_lstm_256_1_1_step_1000.pt
 # put the path to your model checkpoint or final model
-python3.6 ./src/translate.py \
+python3 ./src/translate.py \
 	-model models/yo_adr_bahdanau_lstm_128_1_1_step_100000_release.pt \
 	-src data/test/one_phrase.txt \
 	-output data/test/pred.txt \
