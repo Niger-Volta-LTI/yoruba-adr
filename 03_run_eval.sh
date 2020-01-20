88e25ac1dc729ef6e443e3457aac86bd3067bdd2
ruohoruotsi <ruoho.ruotsi@gmail.com>
2020-01-20 14:35:34 -0800

    [ADD] results for different models based on whether we penalize unk or not

--- 03_run_eval.sh
+++ 03_run_eval.sh
@@ -3,23 +3,29 @@
 
 ### OLDER
 # Accuracy (%): 18.318043811824623
-# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_256_1_1_step_100000.pt
+# Accuracy (%): 27.824060299514038
+FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_256_1_1_step_100000.pt
 
 # Accuracy (%): 48.2125950834122 (does this have yoglobalvoices in it?)
+# Accuracy (%): 72.562893081761
 # FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_sans_iroyin_step_21000_no_kola_mistake.pt
 
 # Accuracy (%): 46.10362051516438 includes yoglobalvoices
+# Accuracy (%): 58.44956748478953
 # FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_sans_iroyin_all_in_take1_step_100000.pt
 
 # Accuracy (%): 88.39084587509386 includes yoglobalvoices
+# Accuracy (%): 91.98233395617463
 # FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_step_90000_release.pt
 
 ## ----
 # Accuracy (%): 41.26538474094871 -- Transformer!!
-# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_transformer_sans_yoglobalvoices_all_in_step_6000.pt
+# Accuracy (%): 48.332813960735436
+# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_transformer_sans_yoglobalvoices_all_in_step_9000.pt
 
 # Accuracy (%): 45.643302536645876  Seq2Seq
-FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_sans_yoglobalvoices_all_in_take2_step_100000.pt
+# Accuracy (%): 54.46011218448115 <-- ignoring unks in reference/target and comuputing anyway
+# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_sans_yoglobalvoices_all_in_take2_step_100000.pt
 
 
 for f in $FILES
