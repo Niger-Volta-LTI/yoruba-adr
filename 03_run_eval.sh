0b348f9ce31224c30e2f84eb309b696f7c4254ba
ruohoruotsi <ruoho.ruotsi@gmail.com>
2020-01-20 17:56:22 -0800

    [ADD] last comments before move

--- 03_run_eval.sh
+++ 03_run_eval.sh
@@ -4,7 +4,11 @@
 ### OLDER
 # Accuracy (%): 18.318043811824623
 # Accuracy (%): 27.824060299514038
-FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_256_1_1_step_100000.pt
+# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_256_1_1_step_100000.pt
+
+# Accuracy (%): 88.39084587509386  # add in details from flash drive w/ all the models.
+# Accuracy (%): 91.98233395617463
+# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_step_90000_release.pt
 
 # Accuracy (%): 48.2125950834122 (does this have yoglobalvoices in it?)
 # Accuracy (%): 72.562893081761
@@ -14,14 +18,11 @@ FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_256_1_1_step_
 # Accuracy (%): 58.44956748478953
 # FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_sans_iroyin_all_in_take1_step_100000.pt
 
-# Accuracy (%): 88.39084587509386 includes yoglobalvoices
-# Accuracy (%): 91.98233395617463
-# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_bahdanau_lstm_128_2_2_step_90000_release.pt
 
 ## ----
 # Accuracy (%): 41.26538474094871 -- Transformer!!
 # Accuracy (%): 48.332813960735436
-# FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_transformer_sans_yoglobalvoices_all_in_step_9000.pt
+FILES=/Users/rebeiro/github/yoruba-adr/models/yo_adr_transformer_sans_yoglobalvoices_all_in_step_9500.pt
 
 # Accuracy (%): 45.643302536645876  Seq2Seq
 # Accuracy (%): 54.46011218448115 <-- ignoring unks in reference/target and comuputing anyway
